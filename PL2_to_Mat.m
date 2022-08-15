a865a94fc22206f95c9a88ee0a084e1f9d215ce5
Chelsea Faber <kasper.chelsea@gmail.com>
2022-08-15 11:15:38 -0700

    Moves spike detection sections to new script
    Goal is to parse long script into shorter, more discrete scripts for specific tasks.

--- PL2_to_Mat.m
+++ PL2_to_Mat.m
@@ -14,12 +14,6 @@ clc;
 
 %    - quick time-frequency spectra?
 
-%    - Automate spike sorting with wave_clus --> how to set default
-%    settings but be able to go back and manually sort after? 
-
-%    - combine sorted spikes into matrix --> ID optical responders &
-%    standardize metric/doc
-
 %    - tidy data procedure to automate data down-sampling, filtering,
 %   epoch-alignment, spike extraction, etc. based upon pre-defined
 %   procedure/parameter saved in ExperimentLog file.
@@ -61,32 +55,6 @@ end
 % files just for Get_spikes. Can do that temporarily in future if necessary
 cd(save_dir)
 save([file_name '.mat'],'data_mat','sr');          
-
-%% Prepare for spike detection
-
-% Save each channel as separate .mat for Get_spikes
-% data = data_mat(k,:);
-% save([file_name '_ch' num2str(k) '.mat'],'data','sr') % save raw broadband data for each channel as .mat
-
-% Create cell array containing all filenames for Get_spikes
-% txt                     = [file_name '_ch' num2str(k+2) '.mat'];
-% save(txt,'data','sr') % save raw broadband data for each channel as .mat
-% batch_txt{k+2}           = txt;
-% 
-% 
-% batch_txt = what; batch_txt = batch_txt.mat;
-% batch_input = [file_name '_batch.txt'];
-% writecell(batch_txt,batch_input);  % save txt file with names of each .mat file for broadband data
-
-%% Spike detection
-Get_spikes(batch_input);
-
-% set parameters for automated spike sorting:
-param.min_clus              = 20;
-param.max_spk               = 50000;
-par.maxtemp                 = 0.251;
-set(0, 'DefaultFigureWindowStyle', 'normal');
-Do_clustering('09172021test2_ch5_spikes.mat'); % test file for now; use 'all' to run all files in current directory
 
 %% Load optical stimulation data
 
