14f2604a2812a601b3b973477df0e5ab599948c9
Chelsea Faber <kasper.chelsea@gmail.com>
2022-08-22 11:38:34 -0700

    Renames vars and removes some calcs from optical stim load

--- PL2_to_Mat.m
+++ PL2_to_Mat.m
@@ -18,7 +18,7 @@ clc;
 %    - convert to function: 
 %       - inputs: 
 %       - array of file names? or just one at a time for now
-%       - list of non-neural data channels desired
+%       - list of non-neural data channels desired (varargin?)
 %    - improve hard-coding of centralized data repository - fullfile?
 
 %% Set data loading/saving directories
@@ -56,27 +56,28 @@ elseif exist([file_name '.mat'],'file') ==0
     [nChan,sampCounts]          = plx_adchan_samplecounts(load_path);
     L                           = sampCounts(1);
     number_of_channels          = 16;
-    data_mat                    = zeros(number_of_channels,L);              % initialize data matrix
+    data                        = zeros(number_of_channels,L);              % initialize data matrix
 
     broadband_ch_names          = {'WB01';'WB02';'WB03';'WB04';'WB05';'WB06';'WB07';'WB08';'WB09';'WB10';'WB11';'WB12';'WB13';'WB14';'WB15';'WB16'};
 
     for k = 1:number_of_channels
 
         [sr, n, ts, fn, ad]     = plx_ad_v(load_path, broadband_ch_names{k});
-        data_mat(k,:)           = ad;
+        data(k,:)               = ad;
     
     end
 
-    save([file_name '.mat'],'data_mat','sr','pl2idx');
+    save([file_name '.mat'],'data','sr','pl2idx');
     fprintf('Data extracted successfully.')
 
 end
 %% Load optical stimulation data
 
+% Check whether requested data channels exist
+
+
+% Load requested data channels
 [Fs_stim, n, ts, fn, data_stim] = plx_ad_v(load_path, 'AI01');
 
-data_stim                       = (data_stim/max(data_stim)*5)';         % normalize & scale to volts
-L_stim                          = size(data_stim,2);                     % length of signal
-time_basis_stim                 = 0:1/Fs_stim:(L_stim-1)/Fs_stim;        % time base
-
-save([file_name '_opto_stims' '.mat'],'data_stim','Fs_stim','L_stim','time_basis_stim'); 
+% Save
+save([file_name '_opto_stims' '.mat'],'data_stim','Fs_stim'); 
