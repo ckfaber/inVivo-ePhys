431aa3678b3e622ac49c870013a51b4e626f93ad
Chelsea Faber <kasper.chelsea@gmail.com>
2022-08-22 17:08:38 -0700

    Loads raw data via relative path
    To minimize hard-coding of raw data paths, calls fullfile to generate relative file path to centralized raw data repo in Dropbox

--- PL2_to_Mat.m
+++ PL2_to_Mat.m
@@ -10,7 +10,7 @@
 % Requirements: 
 % - Plexon Offline SDK: https://plexon.com/software-downloads/#software-downloads-SDKs
 
-clear;
+clear; close all;
 clc;
 
 %% To-do: 
@@ -24,10 +24,13 @@ clc;
 %% Set data loading/saving directories
 
 % Files
-file_name = '2021-09-17_test5';
+file_name   = '2021-09-17_test5';
+
+% Raw data repo - assumes working directory is 'C:\Users\username\MATLAB\'
+load_dir    = fullfile('.','..','\Dropbox (Barrow Neurological Institute)\Mirzadeh Lab Dropbox MAIN\Data\Plexon_Ephys\');
 
 % Hard-code file directories
-load_dir   = 'C:\Users\cfaber\Dropbox (Barrow Neurological Institute)\Mirzadeh Lab Dropbox MAIN\Data\Plexon_Ephys\';
+%load_dir    = 'C:\Users\cfaber\Dropbox (Barrow Neurological Institute)\Mirzadeh Lab Dropbox MAIN\Data\Plexon_Ephys\';
 
 % Dont' change
 load_path   = [load_dir file_name '.pl2'];
@@ -53,13 +56,15 @@ if exist([file_name '.mat'],'file') ==2
 
 elseif exist([file_name '.mat'],'file') ==0
 
+    % Initialize neural data matrix
     [nChan,sampCounts]          = plx_adchan_samplecounts(load_path);
     L                           = sampCounts(1);
     number_of_channels          = 16;
-    data                        = zeros(number_of_channels,L);              % initialize data matrix
+    data                        = zeros(number_of_channels,L);              
 
     broadband_ch_names          = {'WB01';'WB02';'WB03';'WB04';'WB05';'WB06';'WB07';'WB08';'WB09';'WB10';'WB11';'WB12';'WB13';'WB14';'WB15';'WB16'};
 
+    % Loop through neural data channels
     for k = 1:number_of_channels
 
         [sr, n, ts, fn, ad]     = plx_ad_v(load_path, broadband_ch_names{k});
@@ -67,21 +72,7 @@ elseif exist([file_name '.mat'],'file') ==0
     
     end
 
-%     save([file_name '.mat'],'data','sr','pl2idx');
-%     fprintf('Data extracted successfully.')
-
-end
-%% Load optical stimulation data
-
-% Check whether requested data channels exist
-
-
-if exist([file_name '_AI.mat'],'file') ==2
-
-    fprintf('Non-neural data have already been extracted.');
-
-elseif exist([file_name '_AI.mat'],'file') ==0
-
+    % Initialize non-neural aux analog input channels
     L                           = sampCounts(end);                          % MAY BREAK IF OMNIPLEX CONFIG CHANGED
     number_of_channels          = 32;                                       % MAY BREAK IF OMNIPLEX CONFIG CHANGED
     AI_data                     = zeros(number_of_channels,L);              % initialize data matrix
@@ -90,7 +81,7 @@ elseif exist([file_name '_AI.mat'],'file') ==0
                                  'AI09';'AI10';'AI11';'AI12';'AI13';'AI14';'AI15';'AI16';
                                  'AI17';'AI18';'AI19';'AI20';'AI21';'AI22';'AI23';'AI24';
                                  'AI25';'AI26';'AI27';'AI28';'AI29';'AI30';'AI31';'AI32'};
-
+    % Loop through aux analog input channels
     for k = 1:number_of_channels
 
         [AI_sr, n, ts, fn, AI_ad]     = plx_ad_v(load_path, analog_ch_names{k});
@@ -98,14 +89,8 @@ elseif exist([file_name '_AI.mat'],'file') ==0
     
     end
 
-    save([file_name '_AI.mat'],'AI_data','AI_sr');
-    fprintf('AI data extracted successfully.')
+    % Export as .mat 
+    save([file_name '.mat'],'pl2idx','data','sr','AI_data','AI_sr');
+    fprintf('Data extracted successfully.')
 
 end
-
-
-% Load requested data channels
-% [sr_AI, n, ts, fn, data_AI] = plx_ad_v(load_path, 'AI01');
-% 
-% Save
-% save([file_name 'AI' '.mat'],'data_AI','sr_AI'); 
