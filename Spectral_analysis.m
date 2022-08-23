004d81f931d3e0763a502e147428504712731b41
Chelsea Faber <kasper.chelsea@gmail.com>
2022-08-23 14:23:35 -0700

    Initializes spectral analysis WIP script

--- Spectral_analysis.m
+++ Spectral_analysis.m
@@ -1,12 +1,12 @@
 % Spectral analysis
 % Playground for developing spectral analysis pipeline
-%
+% 
 % Basic workflow:
-%
+% 
 % - Low-pass filter OR band-pass filter (hypothesis-dependent)
 % - SSE on requested vs actual filter
 % - Quick vis for quality control
-
+% 
 % Consider when to use:
 % 
 % - Cross-correlograms of channel similarity
@@ -14,4 +14,3 @@
 %   filter-hilbert
 % - LFP snippets around user-requested epoch (spikes, behavioral events, etc)
 % - frequency band power during behavioral epochs (see Korotkova 2017)
-% - 
