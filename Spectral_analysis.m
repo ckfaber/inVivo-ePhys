5fb5c3e9344edb8d3e82ca8039fa122c582330a9
Chelsea Faber <kasper.chelsea@gmail.com>
2022-08-23 14:47:25 -0700

    Adds code to load broadband data from .mat

--- Spectral_analysis.m
+++ Spectral_analysis.m
@@ -14,3 +14,28 @@
 %   filter-hilbert
 % - LFP snippets around user-requested epoch (spikes, behavioral events, etc)
 % - frequency band power during behavioral epochs (see Korotkova 2017)
+
+% INPUTS
+%   filename: name of .mat containing matrix of broadband neural data (output from
+%             PL2_to_Mat())
+%   
+%   [filter type & parameters] - name:value pairs? think through best way
+%             to do this
+%
+%   savepath: path to project directory in which to save?
+%
+% OUTPUTS
+
+%% Hard-code file name
+
+file_name   = '2022-08-16_dmu005_001.mat';
+
+%% Load broadband data
+
+load_dir    = fullfile(userpath,'..','\Dropbox (Barrow Neurological Institute)\Mirzadeh Lab Dropbox MAIN\Data\Plexon_Ephys\Extracted\');
+load([load_dir file_name],'data','sr','pl2idx')
+
+%% Filter 
+
+
+
