48e693bed0ce86dd34981e9d4f991b278ad27245
Chelsea Faber <kasper.chelsea@gmail.com>
2022-09-12 11:51:50 -0700

    Modifies conditional checks for pre-existing files
    Was getting too lengthy/complicated. Now just looks for MUA.mat file, and prompts user if they want to overwrite and do whole analysis over again. 
    
    Also adds some parameters for Get_spikes.

--- Spike_detect.m
+++ Spike_detect.m
@@ -17,91 +17,94 @@
 %   _spikes.mat files to loop through them.
 
 
-filename    = '2022-08-16_dmu005_001';
+filename            = '2022-08-16_dmu005_001';
+w_pre               = 0.5;  % in ms
+w_post              = 2;    % in ms
 
-%% Load raw neural data
+%% Paths to load and save
 
 load_dir    = fullfile(userpath,'..','\Dropbox (Barrow Neurological Institute)\Mirzadeh Lab Dropbox MAIN\Data\Plexon_Ephys\Extracted\');
 save_dir    = fullfile(userpath,'..','\Dropbox (Barrow Neurological Institute)\Mirzadeh Lab Dropbox MAIN\Data\Plexon_Ephys\Processed\');
-raw_data    = load([load_dir filename],'-mat','data','sr');
+savename    = [filename '_MUA' '.mat'];
 
-data_mat    = raw_data.data;
-sr          = raw_data.sr;
-clear raw_data
-
-%% Prepare for spike detection
-
-% Initialize empty data matrix and cell array with file names
-nChan       = size(data_mat,1);
-batch_files = cell(nChan,1);
-
-% Check if temp folder for Get_spikes already exists
-path = fullfile(tempdir,'Get_spikes');
-if exist(path) ~=7
-    mkdir(tempdir,'Get_spikes');
-end
-
-% Check if _spikes.mat files exist already
-pathfiles   = what(path);
-pathfiles   = pathfiles.mat;
-spikefiles  = pathfiles(find(contains(pathfiles,'spikes.mat')));
-rawfiles    = pathfiles(find(~contains(pathfiles,'spikes.mat')));
-
-if size(rawfiles,2) == nChan % MODIFY - CHECK IF FILENAMES MATCH FILENAME
-    prompt = '_spikes.mat files with matching filename detected in tempdir. Repeat spike detection and overwrite existing files?';
+if exist(fullfile(save_dir,savename)) ==2 
+    prompt = [savename ' detected in ' save_dir '. Would you like to repeat spike detection and over-write previous data?'];
     answer = questdlg(prompt, ...
                       'Warning', ...
                       'YES', ...
                       'NO', 'NO')
     switch answer
-        case 'YES'
-
-        % Loop through channels and save each to individual .mat file
-        for chi = 1:nChan
-
-         % To temp folder, save each channel as separate .mat for Get_spikes
-         temp_filename      = [filename '_ch' num2str(chi)];
-         data = data_mat(chi,:);
-         save(fullfile(path,'Get_spikes',temp_filename),'data','sr') % save raw broadband data for each channel as .mat
-         fprintf('Data saved to tempdir, ready for input to Get_spikes.m')
-
-         % Spike detection via Get_spikes
-
-         % if parameters detected in varargin
-         % 
-         %     param.detection = 'both';
-         %     param.w_pre      = w_pre;
-         %     param.w_post     = w_post;
-         % 
-         % end
-
-        cd(fullfile(path,'Get_spikes'))
-        Get_spikes(rawfiles,'par',param);
-
-        end
-
         case 'NO'
-            fprintf('Existing _spikes.mat files located in tempdir.\nType tempdir at the command line to locate for further analysis.\n')
+            fprintf('MUA already performed. Exiting function.')
+            return
+
+        case 'YES'
+            % Load raw neural data
+            raw_data    = load([load_dir filename],'-mat','data','sr');
+    
+            % Format for Get_spikes
+            data_mat    = raw_data.data;
+            sr          = raw_data.sr;
+            nChan       = size(data_mat,1);
+    
+            % Set parameters for Get_spikes
+            param.w_pre         = w_pre/1000 * sr;
+            param.w_post        = w_post/1000 * sr;
+            param.detection     = 'both';
+            param.sr            = sr;
+            clear raw_data
+            fprintf('Raw data extracted')
+
+            % Check if temp folder for Get_spikes already exists
+            path = fullfile(tempdir,'Get_spikes');
+            if exist(path) ~=7
+                mkdir(tempdir,'Get_spikes');
+            end
+
+%             % Check if _spikes.mat files exist already
+%             pathfiles   = what(path);
+%             pathfiles   = pathfiles.mat;
+   
+            % Initialize empty data matrix and cell array with file names
+            rawfiles    = cell(nChan,1);
+            spikefiles  = cell(nChan,1);
+    
+            % Loop through channels
+            for chi = 1:nChan
+        
+                 % To temp folder, save each channel as separate .mat for Get_spikes
+                 temp_filename      = [filename '_ch' num2str(chi)];
+                 rawfiles{chi}      = [temp_filename '.mat'];
+                 spikefiles{chi}    = [temp_filename '_spikes'];
+        
+                 data = data_mat(chi,:);
+                 save(fullfile(path,temp_filename),'data','sr') % save raw broadband data for each channel as .mat
+         
+                 fprintf(['Channel ' num2str(chi) ' data saved to tempdir, ready for input to Get_spikes.m\n'])
+                   
+            end
+
+            cd(path)
+            fprintf('Running Get_spikes\n')
+            Get_spikes(rawfiles,'par',param);
+
+            %Combine separate _spikes files into one big struct. Save to Extracted folder.
+            spikes             = struct;
+            
+            % Loop through _spikes.mat files
+            fprintf("Saving detected spikes from Get_spikes into struct in 'Processed' folder")
+            for chi = 1:size(spikefiles,1);
+            
+                fieldname    = ['ch' num2str(chi)];
+                spikes.(fieldname) = load(spikefiles{chi},'index','threshold','spikes','par');
+            
+            end
+            
+            cd(save_dir)
+            save(savename,'spikes');
+            fprintf([savename ' saved to ' save_dir])
+    
     end
 end
-
-%% Combine separate _spikes files into one big struct. Save to Extracted folder.
-
-% Add check for _MUA.mat before executing
-% look into save_dir if it contains filename_MUA.mat.
-
-% Initialize struct to store spike data
-spikes             = struct;
-
-% Loop through _spikes.mat files
-for chi = 1:size(spikefiles,2);
-
-    fieldname    = ['ch' num2str(chi)];
-    spikes.(fieldname) = load(spikefiles{chi},'index','threshold','spikes','par');
-
-end
-
-cd(save_dir)
-save([filename '_MUA' '.mat'],'spikes');
 
 %% THINK ABOUT INPUT TO DO_CLUSTERING - maybe saving to temporary folder isn't the best way
